4649a47b1e4a489180a8b80a8acb215cbc30c965
ChuecaCristina <56255915+chuecadelc@users.noreply.github.com>
2024-06-13 10:57:39 +0100

    Update experimental_setup.jl
    updated experiment combinations of social reaches and agent proportions

--- experimental_setup.jl
+++ experimental_setup.jl
@@ -42,17 +42,22 @@ social_distances = [single_sr,Poisson_sr,Random_sr,[15,30,0],[30,50,0],[30,40,50
 # number of repetions
 iters = 30 
 
-
-for rep in 1:iters 
-  sc = Social_Circles(rows,cols,population,proportions[1][1],proportions[1][2],Random_sr)
-  # replace run number for current iteration
-  replace!(experiment_trials[!, :run_number], 0 => rep)  
-  replace!(experiment_trials[!, :experiment], 0 => 3) 
-  replace!(df_nodes[!, :run_number], 0 => rep)  
-  replace!(df_nodes[!, :experiment], 0 => 3) 
-  # calculate structural properties
-  node_centrality_cal(sc)
-end
+# combining proportions and social reaches # 
+for (prop_pairs, sr) in zip(keys(proportions), keys(social_distances))
+   for rep in 1:iters 
+     sc = Social_Circles_Proportions(rows,cols,population,proportions[prop_pairs][1],proportions[prop_pairs][2],social_distances[sr])
+     # replacing corresponding run number and experiment in dfs
+     replace!(experiment_trials[!, :run_number], 0 => rep)  
+     replace!(experiment_trials[!, :experiment], 0 => prop_pairs) 
+     replace!(df_nodes[!, :run_number], 0 => rep)  
+     replace!(df_nodes[!, :experiment], 0 => prop_pairs) 
+     # node-level centralities calculations
+     node_centrality_cal(sc)
+     replace!(node_level_df[!, :experiment], 0 => prop_pairs) 
+     replace!(node_level_df[!, :run_number], 0 => rep)  
+     println("Rep $rep")
+   end
+ end
 
 # combine expermental setup and structural properties
 combined = hcat(experiment_trials, df_properties) 
